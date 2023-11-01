b108696130c039b7ea3a9f1572a0b1188158ad1a
NissiChandana <nissi.neerudu@gmail.com>
2023-11-01 18:44:47 -0400

    update-3

--- m1FinPro.js
+++ m1FinPro.js
@@ -4,6 +4,9 @@ let randNumGenerated = 0;
 // This is global variable to track number of attempts made by user
 let guessAttempts = 1; 
 
+// This is global variable to track number of attempts made by user
+let guessesRemaining = 3; 
+
 // This is for alert message to show user
 let userMsg = "";
 
@@ -14,8 +17,9 @@ let userMsg = "";
 function genRandNum() {
     randNumGenerated = Math.floor(Math.random() * 10) + 1;
     guessAttempts = 1;
+    guessesRemaining = 3;
+    guessRemain.value=guessesRemaining;
     guessField.value="";
-    guessRemain.value=3;
     guessField.focus();
     console.log("randNumGenerated = " + randNumGenerated);
 }
@@ -23,20 +27,22 @@ function genRandNum() {
 
 // Function to check if user exceeded attempts. If so show the message 
 // if not show the message returned by the actual routine
-function exceededAttempts(MsgForUser,guessStatus){
-    guessAttempts = guessAttempts + 1;
-    if (guessAttempts == 3 && guessStatus != 'S') { 
+function exceededAttempts(guessStatus){
+    if (guessAttempts == 3 && guessStatus == 'F') { 
         // Last Attempt Message
-        alert("You Used all your guesses. Actual Number : "+randNumGenerated+". Please start Over")
+        //alert("You used all your attempts. Actual number is: " + randNumGenerated);
+        errorMsg.value = "You used all your attempts. Actual number is: " + randNumGenerated
         //document.querySelector(".message").textContent = "correct number";
         genRandNum();
     } else if (guessStatus == 'F'){
-       alert(MsgForUser);
-    }else if (guessStatus == 'F'){
-       //document.querySelector(".message").textContent =""
-        guessRemain.value=4-guessAttempts;
+       //alert(userMsg);
+       errorMsg.value = userMsg;
+       guessesRemaining--;
+       guessRemain.value= guessesRemaining;
+       guessAttempts = guessAttempts + 1;
     } else {
-        alert(MsgForUser);
+        //alert(userMsg);
+        errorMsg.value = userMsg;
         genRandNum();
     }
 
@@ -61,20 +67,24 @@ function startGuessNum() {
            Based on user entry shoe appropriate message
         */
     if (isNaN(numGuessed) || numGuessed < 1 || numGuessed > 10) {
-            //userMsg = "Please Enter A Valid Number between 1 and 10";
-            document.querySelector(".message").textContent = "Please Enter A Valid Number between 1 and 10";
-            exceededAttempts(userMsg, 'F');
-        } else if (numGuessed > randNumGenerated) {
+            userMsg = "Please Enter a Valid Number between 1 and 10";
+            //document.querySelector(".message").textContent = "Please Enter A Valid Number between 1 and 10";
+            exceededAttempts('F');
+    } else if (numGuessed == randNumGenerated) {
+                userMsg ="CONGRATULATIONS! YOU GUESSED IT RIGHT IN " + guessAttempts + " ATTEMPT";
+                exceededAttempts('S');
+            } 
+        else if (numGuessed > randNumGenerated) {
             userMsg ="OOPS SORRY! TRY A SMALLER NUMBER";
-            exceededAttempts(userMsg, 'F');
+            exceededAttempts('F');
         } else if (numGuessed < randNumGenerated) {
             userMsg ="OOPS SORRY! TRY A GREATER NUMBER";
-            exceededAttempts(userMsg, 'F');
-        } else if (numGuessed == randNumGenerated) {
-            userMsg ="CONGRATULATIONS! YOU GUESSED IT RIGHT IN " + guessAttempts + " ATTEMPT";
-            exceededAttempts(userMsg, 'S');
+            exceededAttempts('F');
+        }// else if (numGuessed == randNumGenerated) {
+           // userMsg ="CONGRATULATIONS! YOU GUESSED IT RIGHT IN " + guessAttempts + " ATTEMPT";
+            //exceededAttempts(userMsg, 'S');
         } 
-    }
+    
 
 
 
